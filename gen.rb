aa5c131f647e4d3a6abf205e3d0490c975e7c440
Wojciech A. Koszek <wojciech@koszek.com>
2015-11-02 00:43:32 -0800

    Add images.

--- gen.rb
+++ gen.rb
@@ -1,5 +1,8 @@
 #!/usr/bin/env ruby
 
+require 'rubygems'
+require 'rmagick'
+
 def toFileGen(n)
 	fn = "#{n}.txt"
 	f = File.open("data/#{fn}", "w+")
@@ -15,6 +18,22 @@ toFileGen( 100000) { |n, f| n.times { f.print "d" }}
 toFileGen(     16) { |n, f| n.times { f.print "b" }}
 toFileGen(    128) { |n, f| n.times { f.print "b" }}
 toFileGen(    256) { |n, f| n.times { f.print "b" }}
+
+def genImg(res)
+	["white", "black", "red" ].each do |color|
+		["jpg", "png", "gif"].each do |fmt|
+			fn = "data/#{res}-#{color}.#{fmt}"
+			print "# generating #{fn}\n"
+			`convert -size #{res} xc:#{color} #{fn}`
+		end
+	end
+end
+
+genImg('1x1')
+genImg('10x10')
+genImg('100x100')
+genImg('1000x1000')
+
 
 # add alexa rank pages.
 # add data URL generator
